d529781cfa484df3728e6482f94a108c3b9d83e3
devMachadoA <amachadoamaral2@gmail.com>
2023-11-30 14:15:24 +0000

    js scrollReveal

--- js/menu.js
+++ js/menu.js
@@ -26,7 +26,7 @@ const sr = ScrollReveal({ reset: true });
 sr.reveal(".conf-about", {
     duration: 2500,
     move: 0,
-    // easing: "cubic-bezier(0.5, 0, 0, 1)",
+    easing: "cubic-bezier(0.5, 0, 0, 1)",
 });
 
 sr.reveal(".skills", {
